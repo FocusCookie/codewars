e6125e654e7cd8f3f2191977c1730749d742f093
FocusCookie <stephan_lueddemann@web.de>
2020-08-02 10:50:11 +0200

    dirReduce

--- dirReduc/dirReduc.js
+++ dirReduc/dirReduc.js
@@ -1,5 +1,30 @@
 function dirReduc(arr) {
-  //TODO: FINISH
+  let inWord = arr.join("-");
+
+  const ns = "NORTH-SOUTH";
+  const sn = "SOUTH-NORTH";
+  const ew = "EAST-WEST";
+  const we = "WEST-EAST";
+
+  while (
+    inWord.includes(ns) ||
+    inWord.includes(sn) ||
+    inWord.includes(ew) ||
+    inWord.includes(we)
+  ) {
+    if (inWord.includes(ns)) inWord = inWord.replace(ns, "");
+    if (inWord.includes(sn)) inWord = inWord.replace(sn, "");
+    if (inWord.includes(ew)) inWord = inWord.replace(ew, "");
+    if (inWord.includes(we)) inWord = inWord.replace(we, "");
+
+    // remove all multiple dashes
+    inWord = inWord
+      .split("-")
+      .filter((x) => x !== "")
+      .join("-");
+  }
+
+  return inWord !== "" ? inWord.split("-") : [];
 }
 
 console.log(
